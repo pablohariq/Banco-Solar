758d0530c2025bc6304b78b71f5ea4cc48a6196c
pablohariq <lizamav.p@gmail.com>
2021-08-05 16:59:27 -0400

    agrega las rutas relacionadas con las transferencias

--- consultas.js
+++ consultas.js
@@ -1,4 +1,5 @@
-const {Pool} = require('pg')
+const {Pool, Client} = require('pg')
+const moment = require("moment")
 
 const config = {
     user: "postgres",
@@ -11,20 +12,17 @@ const config = {
 
 const pool = new Pool(config)
 
-const realizarConsulta = (objConsulta) => {
-    return new Promise ((resolve, reject) => {
-        pool.connect(async (error_conexion, client, release) => {
-            try {
-                if (error_conexion) throw (error_conexion)
-                const resultadoConsulta = await client.query(objConsulta)
-                resolve(resultadoConsulta)
-    
-            } catch (error_consulta) {
-                reject(error_consulta)
-            }
-            release()
-        })
-    })
+const realizarConsulta = async (objConsulta) => {
+    const client = await pool.connect()
+    const respuesta = await client.query(objConsulta)
+    client.release()
+
+    if (respuesta.rows){
+        return respuesta
+    }
+    else{
+        throw("Error de conexion")
+    }
 }
 
 const agregarCliente = async (datosCliente) => {
@@ -46,12 +44,79 @@ const obtenerClientes = async () => {
         name: "obtener-clientes",
         text: `SELECT * FROM usuarios`,
     }
+    const {rows: obtencionClientes} = await realizarConsulta(objConsulta)
+    return obtencionClientes
+}
+
+const editarCliente = async (datosCliente) => {
+    const objConsulta = {
+        name: "editar-cliente",
+        text: `UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *`,
+        values: Object.values(datosCliente)
+    }
     try {
-        const {rows: obtencionClientes} = await realizarConsulta(objConsulta)
-        return obtencionClientes
+        const edicionCliente = await realizarConsulta(objConsulta)
+        return edicionCliente
     } catch (error) {
         return error
     }
 }
 
-module.exports = {agregarCliente, obtenerClientes}
+const eliminarCliente = async (id) => { //metodo: eliminacion en cascada 
+    console.log(id)
+    const objConsulta = {
+        text: `DELETE FROM transferencias WHERE emisor = $1 OR receptor = $1;`,
+        values: [id]
+    }
+    const eliminacionTransferencias = await realizarConsulta(objConsulta)
+    objConsulta.text= `DELETE FROM usuarios WHERE id = $1;`
+    console.log(objConsulta)
+    const eliminacionUsuario = await realizarConsulta(objConsulta)
+    return eliminacionUsuario
+}
+
+const hacerTransferencia = async (datosTransferencia) => {
+    datosTransferencia.monto = parseInt(datosTransferencia.monto)
+    const client = await pool.connect()
+    const valoresDescuento = [datosTransferencia.monto, datosTransferencia.emisor]
+    const valoresAbono = [datosTransferencia.monto, datosTransferencia.receptor]
+    const valoresTransferencia = [...Object.values(datosTransferencia), moment().toISOString()]
+    console.log(valoresTransferencia)
+    try{ //emisor, receptor, monto
+        await client.query("BEGIN;")
+        const descuento = await client.query("UPDATE usuarios SET balance = balance - $1 WHERE id = (SELECT id FROM usuarios WHERE nombre = $2) RETURNING *;", valoresDescuento)
+        const abono = await client.query("UPDATE usuarios SET balance = balance + $1 WHERE id = (SELECT id FROM usuarios WHERE nombre = $2) RETURNING *;", valoresAbono)
+        const transferencia = await client.query(`INSERT INTO transferencias(emisor, receptor, monto, fecha) 
+                            VALUES ((SELECT id FROM usuarios WHERE nombre = $1),
+                                    (SELECT id FROM usuarios WHERE nombre = $2), 
+                                    $3, 
+                                    $4) RETURNING *;`, valoresTransferencia)
+        await client.query("COMMIT;")
+        client.release()
+        return transferencia
+    }
+    catch(e){
+        console.log(e)
+        client.query("ROLLBACK;")
+        client.release()
+        throw("Error en la transacción")
+    }
+}
+
+const obtenerTransferencias = async () => {
+    const objConsulta = {
+        name: "consultar-transferencias",
+        text: `SELECT transferencias.id,
+             (SELECT nombre FROM usuarios WHERE usuarios.id = transferencias.emisor) AS emisor, 
+            (SELECT nombre FROM usuarios WHERE usuarios.id = transferencias.receptor) AS receptor, 
+            monto,
+            fecha 
+            FROM transferencias INNER JOIN usuarios ON transferencias.emisor = usuarios.id;`,
+        rowMode: 'array'
+    }
+    const {rows: transferencias} = await realizarConsulta(objConsulta)
+    console.log(transferencias)
+    return transferencias
+}
+
+module.exports = {agregarCliente, obtenerClientes, editarCliente, eliminarCliente, hacerTransferencia, obtenerTransferencias}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const fs = require('fs')
-const {agregarCliente, obtenerClientes} = require("./consultas")
+const {agregarCliente, obtenerClientes, editarCliente, eliminarCliente, hacerTransferencia, obtenerTransferencias} = require("./consultas")
+const url = require('url')
 
 http
 .createServer(async (req, res) => {
@@ -24,13 +25,76 @@ http
     }
 
     if (req.url.startsWith("/usuarios") && req.method == 'GET'){
-        const clientes = await obtenerClientes()
-        res.writeHead(200, {"Content-type": "Application/json"})
-        res.end(JSON.stringify(clientes))
+        try {
+            const clientes = await obtenerClientes()
+            console.log(clientes)
+            res.writeHead(200, {"Content-type": "Application/json"})
+            res.end(JSON.stringify(clientes))
+        } catch (error) {
+            console.log("ruta", error)
+            res.statusCode = 500
+            res.end()
+        }
     }
 
     if (req.url.startsWith("/usuario") && req.method == "PUT"){
-        
+        const id = url.parse(req.url, true).query.id
+        let body = ""
+        req.on("data", (chunk) => {
+            body += chunk
+        })
+        req.on("end", async () => {
+            const datosCliente = JSON.parse(body)
+            datosCliente.id = parseInt(id)
+            console.log(datosCliente)
+            try {
+                await editarCliente(datosCliente)
+                res.statusCode = 200
+            } catch (error) {
+                console.log(error)
+                res.statusCode = 400
+            }
+            res.end()
+        })
+    }
+
+    if (req.url.startsWith("/usuario") && req.method == 'DELETE'){
+        const id = url.parse(req.url, true).query.id
+        try {
+            await eliminarCliente(id)
+            res.statusCode = 204
+            res.end()
+        } catch (error) {
+            res.statusCode = 500
+            res.end(JSON.stringify(error))
+        }
+    }
+
+    if (req.url.startsWith("/transferencia") && req.method == 'POST'){
+        let body = ""
+        req.on("data", (chunk) => {
+            body += chunk
+        })
+
+        req.on("end", async () => {
+            try {
+                const transferencia = await hacerTransferencia(JSON.parse(body))
+                res.statusCode = 201
+                res.end(JSON.stringify(transferencia))
+            } catch (error) {
+                res.statusCode = 500
+                console.log(error)
+                res.end()
+                throw("error en la transaccion")
+            }
+        })
+    }
+
+    if (req.url.startsWith("/transferencias") && req.method == 'GET'){
+        const transferencias = await obtenerTransferencias()
+        console.log(transferencias)
+        res.writeHead(200, {"Content-type": "Application/json"})
+        res.end(JSON.stringify(transferencias))
     }
 
 })
